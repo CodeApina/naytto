c406040d1c3d572f4829a9dccc1dc358d058b255
Henri Sarén <henri.saren@gmail.com>
2024-02-03 07:19:49 +0200

    Refactor keys to use new constants
    Changed Firestore keys to use the new constants to allow easier
    rebuilding of database

--- lib/src/features/authentication/data/link_auth_to_db.dart
+++ lib/src/features/authentication/data/link_auth_to_db.dart
@@ -3,29 +3,31 @@ import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:firebase_core/firebase_core.dart';
-
-  Future<bool>checkDbForUser(uid) async{
-  await Firebase.initializeApp();
-  var db = await FirebaseFirestore.instance;
-  var dbref = db.collection("resident");
-  bool found = false;
-  try{
-    var data = await dbref.where("uid", isEqualTo: uid).get().then((querySnapshot) {
-      if (querySnapshot.docs.isNotEmpty){
-        found = true;
+  class LinkAuthToDb{
+    var db = FirebaseFirestore.instance;
+    Future<bool>checkDbForUser(uid) async{
+      
+      var dbref = db.collection("resident");
+      bool found = false;
+      try{
+        var data = await dbref.where("uid", isEqualTo: uid).get().then((querySnapshot) {
+          if (querySnapshot.docs.isNotEmpty){
+            found = true;
+          }
+      }).onError((error, stackTrace) {
+        print("Error checking db for user");
+      });
+      } on Exception catch(e){
+        throw Exception('Exeption: $e');
       }
+      return found;
+    } 
 
-  }).onError((error, stackTrace) {
-    print("Error checking db for user");
-  });
-  } on Exception catch(e){
-    throw Exception('Exeption: $e');
-  }
-  return found;
-}
+    Future<bool>storeUserInDB(uid) async{
+      var dbref = db.collection("resident");
 
-  Future<bool>storeUserInDB(uid) async{
-    return false;
+      return false;
+    }
   }
 
 
