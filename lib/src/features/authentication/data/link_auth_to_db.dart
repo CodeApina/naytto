995f1e4cafed99aace470f97d7311ca04502aed7
Henri Sarén <henri.saren@gmail.com>
2024-02-03 08:00:21 +0200

    feat: Add storeUserInDB function and documentation
    Added storeUserInDB function to store user data in the database and
    documented functions in the file

--- lib/src/features/authentication/data/link_auth_to_db.dart
+++ lib/src/features/authentication/data/link_auth_to_db.dart
@@ -1,32 +1,50 @@
 import 'dart:async';
-
+import '../../../constants/firestore_constants.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:firebase_core/firebase_core.dart';
   class LinkAuthToDb{
     var db = FirebaseFirestore.instance;
+
+    /// Checks residents collection for user
     Future<bool>checkDbForUser(uid) async{
       
-      var dbref = db.collection("resident");
+      var dbref = db.collection(FirestoreCollections.residents);
       bool found = false;
       try{
-        var data = await dbref.where("uid", isEqualTo: uid).get().then((querySnapshot) {
+        var data = await dbref.where(FirestoreFields.residentResidentId, isEqualTo: uid).get().then((querySnapshot) {
           if (querySnapshot.docs.isNotEmpty){
             found = true;
           }
       }).onError((error, stackTrace) {
-        print("Error checking db for user");
+        print("Error: $error  \n $stackTrace");
       });
       } on Exception catch(e){
         throw Exception('Exeption: $e');
       }
       return found;
     } 
-
-    Future<bool>storeUserInDB(uid) async{
-      var dbref = db.collection("resident");
-
-      return false;
+    /// Stores user in the database.
+    ///
+    /// Requires Map<String,Dynamic>[uid, email, firstName, lastName, tel, apartmentNumber].
+    Future<bool>storeUserInDB(Map<String,dynamic>mapOfData) async{
+      var dbref = db.collection(FirestoreCollections.apartments);
+      var apartmentId = await dbref.where(FirestoreFields.apartmentsNumber, isEqualTo: mapOfData["apartmentNumber"]);
+      dbref = db.collection(FirestoreCollections.residents);
+      return await dbref.add({
+        FirestoreFields.residentResidentId: mapOfData["uid"],
+        FirestoreFields.residentEmail: mapOfData["email"],
+        FirestoreFields.residentFirstName: mapOfData["firstName"],
+        FirestoreFields.residentLastName: mapOfData["lastName"],
+        FirestoreFields.residentApartmentId: apartmentId,
+        FirestoreFields.residentTel: mapOfData["tel"],
+      }).then((value) {
+        return true;
+      }).onError((error, stackTrace) {
+        print("Error: $error  \n $stackTrace");
+        return false;
+      });
+      
     }
   }
 
