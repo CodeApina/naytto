2adbf5225a07baf344960b2a4531fef9d74e0468
Henri Sarén <henri.saren@gmail.com>
2024-02-03 08:49:38 +0200

    feat: Add linkResidentToUser function
    linkResidentToUser function checks the residents collection to see if
    user is present, if the user is found then userID is added to the
    resident to link them together.

--- lib/src/features/authentication/data/link_auth_to_db.dart
+++ lib/src/features/authentication/data/link_auth_to_db.dart
@@ -12,15 +12,15 @@ import 'package:firebase_core/firebase_core.dart';
       var dbref = db.collection(FirestoreCollections.residents);
       bool found = false;
       try{
-        var data = await dbref.where(FirestoreFields.residentResidentId, isEqualTo: uid).get().then((querySnapshot) {
+        await dbref.where(FirestoreFields.residentResidentId, isEqualTo: uid).get().then((querySnapshot) {
           if (querySnapshot.docs.isNotEmpty){
             found = true;
           }
       }).onError((error, stackTrace) {
         print("Error: $error  \n $stackTrace");
       });
-      } on Exception catch(e){
-        throw Exception('Exeption: $e');
+      } on Exception catch(error){
+        throw Exception('Exeption: $error');
       }
       return found;
     } 
@@ -28,15 +28,13 @@ import 'package:firebase_core/firebase_core.dart';
     ///
     /// Requires Map<String,Dynamic>[uid, email, firstName, lastName, tel, apartmentNumber].
     Future<bool>storeUserInDB(Map<String,dynamic>mapOfData) async{
-      var dbref = db.collection(FirestoreCollections.apartments);
-      var apartmentId = await dbref.where(FirestoreFields.apartmentsNumber, isEqualTo: mapOfData["apartmentNumber"]);
-      dbref = db.collection(FirestoreCollections.residents);
+      var dbref = db.collection(FirestoreCollections.residents);
       return await dbref.add({
         FirestoreFields.residentResidentId: mapOfData["uid"],
         FirestoreFields.residentEmail: mapOfData["email"],
         FirestoreFields.residentFirstName: mapOfData["firstName"],
         FirestoreFields.residentLastName: mapOfData["lastName"],
-        FirestoreFields.residentApartmentId: apartmentId,
+        FirestoreFields.residentApartmentId: mapOfData["apartmentNumber"],
         FirestoreFields.residentTel: mapOfData["tel"],
       }).then((value) {
         return true;
@@ -44,7 +42,60 @@ import 'package:firebase_core/firebase_core.dart';
         print("Error: $error  \n $stackTrace");
         return false;
       });
+    }
+
+    ///Links resident to user in db
+    ///
+    ///Requires uid AND both first AND last name OR one other parameter
+    Future<bool>linkResidentToUser(uid, {email, apartmentNumber, tel, firstName, lastName}) async{
+      assert((email != null || apartmentNumber != null || tel != null || firstName != null && lastName != null), "First and last name or one other parameter required");
+      var dbref = db.collection(FirestoreCollections.residents);
+      String? residentId;
       
+      if(email != null){
+        await dbref.where(FirestoreFields.residentEmail, isEqualTo: email).get().then((querySnapshot) {
+          if(querySnapshot.docs.isNotEmpty){
+            residentId = querySnapshot.docs[0].id;
+          }
+        }).onError((error, stackTrace){
+          print("Error: $error \n $stackTrace");
+         });
+      }
+      if(apartmentNumber != null && residentId == null){
+        await dbref.where(FirestoreFields.residentApartmentId, isEqualTo : apartmentNumber).get().then((querySnapshot) {
+          if (querySnapshot.docs.isNotEmpty){
+            residentId = querySnapshot.docs[0].id; 
+          }
+        });
+      }
+      if (tel != null && residentId == null){
+        await dbref.where(FirestoreFields.residentTel, isEqualTo : tel).get().then((querySnapshot) {
+          if (querySnapshot.docs.isNotEmpty){
+            residentId = querySnapshot.docs[0].id; 
+          }
+        }).onError((error, stackTrace) {
+          print("Error: $error \n $stackTrace");
+        });
+      }
+      if(firstName != null && lastName != null && residentId == null){
+        await dbref.where(FirestoreFields.residentFirstName, isEqualTo : firstName).where(FirestoreFields.residentLastName, isEqualTo: lastName).get().then((querySnapshot) {
+          if (querySnapshot.docs.isNotEmpty){
+            residentId = querySnapshot.docs[0].id; 
+          }
+        }).onError((error, stackTrace) {
+          print("Error: $error \n $stackTrace");
+        });
+      }
+      if (residentId == null){
+        print("Error: User not found");
+        return false;
+      }
+      return await dbref.doc(residentId).set({FirestoreFields.residentResidentId : uid}).then((value) {
+        return true;
+      },).onError((error, stackTrace) {
+        print("Error: $error \n $stackTrace");
+        return false;
+      });
     }
   }
 
