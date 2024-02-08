e946ab518aab35b0b67e8d45de02e10c6dbc5e4f
Henri Sarén <henri.saren@gmail.com>
2024-02-08 04:31:12 +0200

    refactor: Refactored code and split it into functions

--- lib/src/utilities/housing_cooperative_creator.dart
+++ lib/src/utilities/housing_cooperative_creator.dart
@@ -1,12 +1,10 @@
 import 'dart:math';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:naytto/src/constants/firestore_constants.dart';
 
+
 class HousingCooperativeCreator{
   var db = FirebaseFirestore.instance;
-  static List<String> emailProviders = ["luukku.com", "gmail.com", "hotmail.fi", "outlook.com"];
-
   ///Requires the name for the housing cooperative, the address and the amount of apartments you want the building to have
   ///
   ///Creates a new housing cooperative collection in the firestore database filled with random residents and data
@@ -15,15 +13,15 @@ class HousingCooperativeCreator{
     return db.collection(FirestoreCollections.housingCooperative).doc(housingCooperativeName).set({
       FirestoreFields.housingCooperativeAddress: address}).then((value)
       {
-        for(int i = 1; i <= apartmentNumber; i++){
+        for(int i = 0; i < apartmentNumber; i++){
           db.collection(FirestoreCollections.housingCooperative).doc(housingCooperativeName).collection(FirestoreCollections.residents).add({
-          FirestoreFields.residentApartmentNumber: "A$i",
-          FirestoreFields.residentEmail: "${persons[i].firstName}.${persons[i].lastName}@${emailProviders[Random().nextInt(emailProviders.length)]}",
+          FirestoreFields.residentApartmentNumber: "A${i + 1}",
+          FirestoreFields.residentEmail: persons[i].email,
           FirestoreFields.residentFirstName: persons[i].firstName,
           FirestoreFields.residentLastName: persons[i].lastName,
           FirestoreFields.residentTel: persons[i].phoneNumber
         }).then((documentSnapshot) {
-          return db.collection(FirestoreCollections.housingCooperative).doc(housingCooperativeName).collection(FirestoreCollections.apartments).doc("A$i").collection(FirestoreCollections.apartmentsResidents).doc().set({
+          return db.collection(FirestoreCollections.housingCooperative).doc(housingCooperativeName).collection(FirestoreCollections.apartments).doc("A${i + 1}").collection(FirestoreCollections.apartmentsResidents).doc().set({
             FirestoreFields.apartmentsResidentsReference: "/${FirestoreCollections.housingCooperative}/$housingCooperativeName/${FirestoreCollections.residents}/${documentSnapshot.id}"
           });
           });
@@ -44,26 +42,47 @@ class HousingCooperativeCreator{
     });
   }
 }
-class PersonGenerator{
 
-  static List<String> firstNames = ["Juhani", "Johannes", "Olavi", "Antero", "Tapani", "Kalevi", "Tapio", "Matti", "Mikael", "Ilmari", "Maria", "Helena", "Johannna", "Anneli", "Kaarina", "Marjatta", "Anna", "Liisa", "Annikki", "Hannele"];
-  static List<String> lastNames = ["Korhonen", "Virtanen", "Mäkinen", "Nieminen", "Mäkelä", "Hämäläinen", "Laine", "Heikkinen", "Koskinen", "Järvinen", "Lehtonen", "Lehtinen", "Saarinen", "Niemi", "Salminen", "Heinone", "Heikkilä", "Kinnunen", "Salonen", "Turunen"];
-  static List<String> phoneNumberPrefix = ["044", "050", "040"];
+/// Creates new residents with random names chosen from pre generated lists
+class PersonGenerator{
   /// Requires the amount of people to create
   List<Person> personListGenerator(int amountNeeded){
-    List<Person> persons = [Person("test","test","test")];
-    for (int i = 1; i <= amountNeeded; i++){
-      String phoneNumber = phoneNumberPrefix[Random().nextInt(phoneNumberPrefix.length)];
-      for(int i = 0; i < 7; i++){
-        phoneNumber = phoneNumber + Random().nextInt(10).toString();
-      }
-      bool isUnique = false;
-    List<String>names = nameGenerator(persons);
-      persons.add(Person(names[0], names[1], phoneNumber));
+    List<Person> persons = List.empty(growable: true);
+    // Creates phonenumber for the person
+    for (int i = 0; i < amountNeeded; i++){
+      String phoneNumber = phoneNumberGenerator();
+      List<String>names = nameGenerator(persons);
+      String email = emailGenerator(names);
+      persons.add(Person(names[0], names[1], phoneNumber, email));
     }
     return persons;
   }
+  /// Generates email address for resident
+  /// 
+  /// Requires List[(0) first name, (1) last name] 
+  String emailGenerator(names){
+    List<String> emailProviders = ["luukku.com", "gmail.com", "hotmail.fi", "outlook.com"];
+    String email = "${names[0]}.${names[1]}@${emailProviders[Random().nextInt(emailProviders.length)]}";
+    return email;
+  }
+
+  /// Generates a random phone number chooses one prefix and adds 7 numbers to it creating a full phone number
+  String phoneNumberGenerator(){
+    List<String> phoneNumberPrefix = ["044", "050", "040"];
+    String phoneNumber = phoneNumberPrefix[Random().nextInt(phoneNumberPrefix.length)];
+      for(int i = 0; i < 7; i++){
+        phoneNumber = phoneNumber + Random().nextInt(10).toString();
+      }
+    return phoneNumber;
+  }
+  /// Generates name for person and checks that the name isn't a duplicate of a name already on the list
+  /// 
+  /// Takes a list of Persons created so far
+  /// 
+  /// Returns List[(0)first name, (1)last name]
   List<String>nameGenerator(List<Person> persons){
+    List<String> firstNames = ["Juhani", "Johannes", "Olavi", "Antero", "Tapani", "Kalevi", "Tapio", "Matti", "Mikael", "Ilmari", "Maria", "Helena", "Johannna", "Anneli", "Kaarina", "Marjatta", "Anna", "Liisa", "Annikki", "Hannele"];
+    List<String> lastNames = ["Korhonen", "Virtanen", "Mäkinen", "Nieminen", "Mäkelä", "Hämäläinen", "Laine", "Heikkinen", "Koskinen", "Järvinen", "Lehtonen", "Lehtinen", "Saarinen", "Niemi", "Salminen", "Heinone", "Heikkilä", "Kinnunen", "Salonen", "Turunen"];
     List<String>names = [firstNames[Random().nextInt(firstNames.length)], lastNames[Random().nextInt(lastNames.length)]];
     for(Person person in persons){
       if(person.firstName == names[0] && person.lastName == names[1]){
@@ -74,9 +93,13 @@ class PersonGenerator{
   }
 }
 
+/// Basic information template for generated resident
+/// 
+/// Stores first and last name and phone number
 class Person{
   String firstName = "";
   String lastName = "";
   String phoneNumber = "";
-  Person(this.firstName, this.lastName, this.phoneNumber);
+  String email = "";
+  Person(this.firstName, this.lastName, this.phoneNumber, this.email);
 }
