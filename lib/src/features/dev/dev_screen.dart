20e185d3f4c89088e442d02c070d83d6c02071be
Henri Sarén <henri.saren@gmail.com>
2024-02-08 04:30:01 +0200

    chore: Fix indentation in the file

--- lib/src/features/dev/dev_screen.dart
+++ lib/src/features/dev/dev_screen.dart
@@ -33,57 +33,58 @@ class DevScreen extends ConsumerWidget {
                       onPressed: () async {
                         await showDialog(
                           context: context,
-                         builder: (context) => AlertDialog(
-                          content: Stack(
-                            clipBehavior: Clip.none,
-                            children: <Widget>[
-                              Positioned(
-                                right: -40,
-                                top:  -40,
-                                child: InkResponse(
-                                  onTap: (){
-                                    Navigator.of(context).pop();
-                                  },
-                                  child: const CircleAvatar(
-                                    backgroundColor: Colors.red,
-                                    child: Icon(Icons.close),
+                          builder: (context) => AlertDialog(
+                            content: Stack(
+                              clipBehavior: Clip.none,
+                              children: <Widget>[
+                                Positioned(
+                                  right: -40,
+                                  top:  -40,
+                                  child: InkResponse(
+                                    onTap: (){
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: const CircleAvatar(
+                                      backgroundColor: Colors.red,
+                                      child: Icon(Icons.close),
+                                    ),
                                   ),
                                 ),
-                              ),
-                              Form(
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: <Widget>[
-                                    const Text("Housing cooperative name:"),
-                                    Padding(
-                                      padding: const EdgeInsets.all(8),
-                                      child: TextFormField(controller: textController1),
-                                    ),
-                                    const Text("Housing cooperative address:"),
-                                    Padding(
-                                      padding: const EdgeInsets.all(8),
-                                      child: TextFormField(controller: textController2),
-                                    ),
-                                    const Text("Number of apartments wanted:"),
-                                    Padding(
-                                      padding: const EdgeInsets.all(8),
-                                      child: TextFormField(controller: textController3,),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.all(8),
-                                      child: ElevatedButton(
-                                        child: const Text("Submit"),
-                                        onPressed: (){
-                                          HousingCooperativeCreator().CreateNewHousingCooperative(textController1.text,textController2.text,int.parse(textController3.text));
-                                        },
+                                Form(
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: <Widget>[
+                                      const Text("Housing cooperative name:"),
+                                      Padding(
+                                        padding: const EdgeInsets.all(8),
+                                        child: TextFormField(controller: textController1),
                                       ),
-                                    )
-                                  ],
-                                ),
-                              )
-                            ],)
-                         ));
-                        
+                                      const Text("Housing cooperative address:"),
+                                      Padding(
+                                        padding: const EdgeInsets.all(8),
+                                        child: TextFormField(controller: textController2),
+                                      ),
+                                      const Text("Number of apartments wanted:"),
+                                      Padding(
+                                        padding: const EdgeInsets.all(8),
+                                        child: TextFormField(controller: textController3,),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.all(8),
+                                        child: ElevatedButton(
+                                          child: const Text("Submit"),
+                                          onPressed: (){
+                                            HousingCooperativeCreator().CreateNewHousingCooperative(textController1.text,textController2.text,int.parse(textController3.text));
+                                          },
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                )
+                              ],
+                            )
+                          )
+                        );
                       },
                       child: const Text('Create Housing Cooperative')),
                   TextButton(
