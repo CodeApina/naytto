4573c6eff631b15886933791a5572d6eaa842f4c
HenriMykkanen <144138353+HenriMykkanen@users.noreply.github.com>
2024-02-14 11:59:36 +0200

    refactor: Removed unnecessary provider
    Removed housingCooperativeNameProvider because that data is already stored in the AppUser and accessible from AppUser.provider

--- lib/src/features/home/data/announcement_repository.dart
+++ lib/src/features/home/data/announcement_repository.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:naytto/src/constants/firestore_constants.dart';
 import 'package:naytto/src/features/authentication/domain/app_user.dart';
-import 'package:naytto/src/features/authentication/domain/housing_cooperative.dart';
 import 'package:naytto/src/features/home/domain/announcement.dart';
 
 final announcementsProvider = StreamProvider.autoDispose<List<Announcement>>(
@@ -10,7 +9,7 @@ final announcementsProvider = StreamProvider.autoDispose<List<Announcement>>(
     final _firestore = FirebaseFirestore.instance;
 
     final String housingCooperativeName =
-        await ref.watch(housingCooperativeNameProvider.future);
+        ref.watch(AppUser().provider).housingCooperative;
 
     yield* _firestore
         .collection(FirestoreCollections.housingCooperative)
