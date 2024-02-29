5457bf6c130c835f035abcbdfeea93835d326214
Olli-Ville <133214845+Olli-Ville@users.noreply.github.com>
2024-02-29 17:05:58 +0200

    Fix. bookings sorting fixed, no more dublicates

--- lib/src/features/home/data/bookings_homescreen_repository.dart
+++ lib/src/features/home/data/bookings_homescreen_repository.dart
@@ -29,16 +29,19 @@ final apartmentsBookingsProvider = StreamProvider.autoDispose<List<Bookings>>(
         //     .toList());
 
         //Chatgpt:s Wild sorting system number II
-        //Shows ALL saunas first and then sorts laundy with timestamp
+        //Shows ALL saunas first and then sorts others with timestamp
         .snapshots()
         .map((querySnapshot) {
       final documents = querySnapshot.docs;
       final saunaBookings = <Bookings>[];
+      final allBookings = <Bookings>[];
 
       for (final doc in documents) {
         final booking = Bookings.fromMap(doc.data(), doc.id);
         if (booking.type == "sauna") {
           saunaBookings.add(booking);
+        } else {
+          allBookings.add(booking);
         }
       }
       saunaBookings.sort((a, b) {
@@ -48,26 +51,14 @@ final apartmentsBookingsProvider = StreamProvider.autoDispose<List<Bookings>>(
           return 0;
         }
       });
-      final allBookings =
-          documents.map((doc) => Bookings.fromMap(doc.data(), doc.id)).toList();
-
       allBookings.sort((a, b) {
-        if (a.type == "laundry" && b.type == "laundry") {
-          // Sort by timestamp if available
-          return (a.timestamp != null && b.timestamp != null)
-              ? a.timestamp!.compareTo(b.timestamp!)
-              : 0;
-        } else if (a.type == "sauna" && b.type == "laundry") {
-          return -1; // "sauna" comes before "laundry"
-        } else if (a.type == "laundry" && b.type == "sauna") {
-          return 1; // "laundry" comes after "sauna"
+        if (a.timestamp != null && b.timestamp != null) {
+          return a.timestamp!.compareTo(b.timestamp!);
         } else {
-          return 0; // Maintain order for other types
+          return 0;
         }
       });
-
       final result = [...saunaBookings, ...allBookings];
-
       return result.take(showbookings).toList();
     });
   },
