6b4bd1b212e38d26c1bc10a209ab5279c7b92e47
HenriMykkanen <144138353+HenriMykkanen@users.noreply.github.com>
2024-03-25 15:35:43 +0200

    fix: laundry screen theme updated to match rest of the app

--- lib/src/constants/theme.dart
+++ lib/src/constants/theme.dart
@@ -16,7 +16,7 @@ ThemeData getAppTheme(BuildContext context) {
     extensions: const <ThemeExtension<CustomColors>>[
       CustomColors(
         color1: Color.fromARGB(255, 255, 255, 255),
-        color2: Color.fromARGB(200, 153, 101, 97),
+        color2: Color.fromRGBO(0, 124, 124, 1.0),
         color3: Color.fromARGB(234, 255, 255, 255),
       ),
     ],
